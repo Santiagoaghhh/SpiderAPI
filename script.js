7f39461f2093f6c09d16496ff9862560d6eb8a34
Santiago Aguilar Vesga <155168125+Santiagoaghhh@users.noreply.github.com>
2025-05-28 15:54:21 +0000

    feat: :rocket: adding api

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const API = 'https://6835b7e7cd78db2058c2d331.mockapi.io/Spidercostumes';
 
 const containerTotal = document.querySelector('.container')
 const containerInyect = document.querySelector('.container--inyect')
+
+fetch(API)
+.then (respuesta => respuesta.json())
+.then (data => {
+    const spiderSignal = data;
+    for(i=0; i<spiderSignal.lenght; i++){
+        
+    }
+})
