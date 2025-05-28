e4520c55e101ff9bd319331df02fb76deb2ece4d
Santiago Aguilar Vesga <155168125+Santiagoaghhh@users.noreply.github.com>
2025-05-28 20:00:16 +0000

    feat: :rocket: spiderman wii

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API = 'https://6835b7e7cd78db2058c2d331.mockapi.io/Spidercostumes';
-
+const editcontainer = document.getElementById('inyectable')
 const containerTotal = document.querySelector('.container')
 const containerInyect = document.querySelector('.container--inyect')
 
@@ -7,7 +7,16 @@ fetch(API)
 .then (respuesta => respuesta.json())
 .then (data => {
     const spiderSignal = data;
-    for(i=0; i<spiderSignal.lenght; i++){
-        
-    }
+    for(i=0; i<spiderSignal.length; i++){
+        editcontainer.innerHTML += `
+        <div class="container--inyect">
+                <div class="container--inyect__img">
+                    <img src="${spiderSignal[i].img}" alt="">
+                </div>
+                <div class="container--inyect__info">
+                    <h3>Name: <span>${spiderSignal[i].name}</span></h3>
+                    <h3>Year: <span>1962</span> </h3>
+                </div>
+        `
+      }
 })
