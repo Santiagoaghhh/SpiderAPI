65d60f203925d5c11cbe1a7c9eb6c8633a9b5f3b
Santiagoaghhh <santaguilar17@gmail.com>
2025-05-28 10:03:16 -0500

    feat: :fire: Adding code

---
+++ script.js
@@ -0,0 +1,4 @@
+const API = 'https://6835b7e7cd78db2058c2d331.mockapi.io/Spidercostumes';
+
+const containerTotal = document.querySelector('.container')
+const containerInyect = document.querySelector('.container--inyect')
